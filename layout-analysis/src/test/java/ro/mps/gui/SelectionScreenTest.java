509444eee927b8e4ac65ed7003b47929dc874e7c
Alexandru Burghelea <iceman.ftg@gmail.com>
2012-11-15 13:30:06 +0200

    moved running of SelectionScreen to a Test

---
+++ layout-analysis/src/test/java/ro/mps/gui/SelectionScreenTest.java
@@ -0,0 +1,34 @@
+package ro.mps.gui;
+
+
+import junit.framework.TestCase;
+import org.junit.Assert;
+import ro.mps.properties.Properties;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
+
+/**
+ * User: Alexandru Burghelea
+ * Date: 11/15/12
+ * Time: 1:01 PM
+ */
+public class SelectionScreenTest extends TestCase {
+
+
+    /**
+     * Testing if gui starts.
+     *
+     * @throws Exception
+     */
+    public void testGuiStarts() throws Exception {
+        EventQueue.invokeAndWait(new Runnable() {
+            public void run() {
+                SelectionScreen ss = new SelectionScreen();
+            }
+        });
+
+        Assert.assertTrue("GUI not loaded", true);
+    }
+}
